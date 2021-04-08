5508312f54a7698503c043e9de5cbc0dcd3cbc27
Alex O'Keeffe <alexwokeeffe@gmail.com>
2021-04-08 21:11:51 +0100

    first commit

---
+++ components/App/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="pt-7">
+    <div class="text-center">
+      <div class="logo" />
+      <h1 class="header-title mb-7">The Crafty Midwife UK</h1>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.logo {
+  height: 140px;
+  width: 140px;
+  background-image: url("~assets/images/craftymidwifelogo.jpg");
+  background-position: center;
+  background-size: contain;
+  margin: 0 auto;
+}
+
+.header-title {
+  font-family: "Boulevard", sans-serif;
+  font-size: 3rem;
+  font-weight: 300;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col col-12 col-6--s col-3--m square">
+        <a
+          href="https://www.instagram.com/thecraftymidwifeuk/"
+          target="_blank"
+          rel="noopener"
+          ><img src="~assets/images/ladybird.jpg" alt="ladybird"
+        /></a>
+      </div>
+      <div class="col col-12 col-6--s col-3--m square">
+        <a
+          href="https://www.instagram.com/thecraftymidwifeuk/"
+          target="_blank"
+          rel="noopener"
+          ><img src="~assets/images/eastereggs.jpg" alt="Easter Eggs"
+        /></a>
+      </div>
+      <div class="col col-12 col-6--s col-3--m square">
+        <a
+          href="https://www.instagram.com/thecraftymidwifeuk/"
+          target="_blank"
+          rel="noopener"
+          ><img src="~assets/images/savethedate.jpg" alt="Save the date"
+        /></a>
+      </div>
+      <div class="col col-12 col-6--s col-3--m square">
+        <a
+          href="https://www.instagram.com/thecraftymidwifeuk/"
+          target="_blank"
+          rel="noopener"
+          ><img src="~assets/images/sloth.jpg" alt="Sloth"
+        /></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
